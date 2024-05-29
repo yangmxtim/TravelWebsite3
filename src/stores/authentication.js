af6b5deefdb8a11de1c64bd2aff8692a9c07d652
pepperwarrior <flyingrush@gmail.com>
2024-05-29 09:22:31 +0800

    git test, dont merge

--- src/stores/authentication.js
+++ src/stores/authentication.js
@@ -3,11 +3,11 @@ import { defineStore } from 'pinia'
 
 export const useAuthenticationStore = defineStore('authentication', () => {
   const credCredentials = ref(0)
-  // const doubleCount = computed(() => count.value * 2)
+  const doubleCount = computed(() => count.value * 2)
   // function increment() {
   //   count.value++
   // }
-
+  
   // return { count, doubleCount, increment }
   return {credCredentials}
 })
