47847d44ed9a9f9d2ddd29ff5ebcc6d75c4f3a9b
yangmxtim <yangmx.tim@gmail.com>
2024-05-31 11:55:05 +0800

    移除首頁登入 顯示登入登出

--- src/views/Layout/components/HeaderLogout.vue
+++ src/views/Layout/components/HeaderLogout.vue
@@ -1,39 +1,43 @@
 <script setup>
-// import axios from 'axios';
-// import { ref } from 'vue';
+import axios from 'axios';
+import { ref } from 'vue';
 
-// // 定义一个响应状态的变量以处理请求响应
-// const responseStatus = ref(null);
 
-// // 发送POST请求的函数
-// const sendLogoutRequest = async () => {
-//   try {
-//     const response = await axios.post('http://localhost:8080/auth/logout', null, {
-//       withCredentials: true,
-//     });
-//     responseStatus.value = response.data;
-//     console.log(response.data); 
-//     alert(response.data);
-//     window.location.replace("/");
-//     window.location.reload();
+// 定义一个响应状态的变量以处理请求响应
+const responseStatus = ref(null);
 
-//   } catch (error) {
-//     console.error('Error during the POST request:', error);
-//     responseStatus.value = 'Request failed';
-//   }
-// };
+// 发送POST请求的函数
+const sendLogoutRequest = async () => {
+  try {
+    const response = await axios.post('http://localhost:8080/auth/logout', null, {
+      withCredentials: true,
+    });
+    responseStatus.value = response.data;
+    console.log(response.data); 
+    alert("登出"+response.data);
+    window.location.replace("/");
+    window.location.reload();
+    
+
+  } catch (error) {
+    console.error('Error during the POST request:', error);
+    responseStatus.value = 'Request failed';
+  }
+};
 
 </script>
 
 <template>
   <div>
     <div>
-    <!-- <button @click="sendLogoutRequest" class="fa-solid fa-skull"> -->
-        <a href="../../memberinfo">
-            <i class="fa-regular fa-poo icon-large" @click="sendLogoutRequest" style="vertical-align: bottom;"></i>
-        </a>
+    <button @click="sendLogoutRequest" >
+        <!-- <a href="../../memberinfo"> -->
+            <i class="fa-solid fa-user icon-large" style="vertical-align: bottom;"></i>
+            <!-- <i class="fa-regular fa-poo icon-large" @click="sendLogoutRequest" style="vertical-align: bottom;"></i> -->
+        <!-- </a> -->
         
-    <!-- </button> -->
+    </button>
+    <div class="layer"></div>
   </div>
     
   </div>
@@ -42,7 +46,7 @@
 <style scoped >
 .icon-large {
     font-size: 21px;
-    padding: 5.5px; /* 设置你想要的padding值 */
+    padding: 5.5px; 
 }
 
 </style>
