902049916b014900a6b3aa3a8dc19fe4372de3f5
lukee <lukevyhe7887@gmail.com>
2024-06-09 15:51:02 +0800

    小修商品葉面

--- src/views/Product/components/ProductInformation.vue
+++ src/views/Product/components/ProductInformation.vue
@@ -1,13 +1,9 @@
 <template>
      <div class="container">
             <h4>設施</h4>
-            <p>{設施}</p><br>
+            <p>{{product.facility}}</p><br>
             <h4>資訊</h4>
             <div class="row">
-                <div class="col-9">
-                    <p>入住時間：{入住時間}&nbsp; 退房時間：{退房時間}</p>
-                    <p>附近地標：{附近地標}</p>
-                </div>
                 <div class="col-3">
                     <div class="map"><br><br><br><br>
                         <button type="button" class="btn btn-outline-primary">查看地圖</button>
@@ -16,6 +12,17 @@
             </div>
         </div>
 </template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  product: {
+    type: Object,
+    required: true
+  }
+});
+</script>
 
 <style scoped>
 /* 地圖 */
