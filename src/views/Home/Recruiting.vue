4ea4b598c4f330bccea7629f5b42455b8fe17e9d
wenyenyeh <angel200041008@gmail.com>
2024-06-13 16:29:01 +0800

    增加人才招募頁、header圖標

---
+++ src/views/Home/Recruiting.vue
@@ -0,0 +1,306 @@
+<script setup>
+
+import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
+import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
+</script>
+
+<template>
+    <LayoutHeader />
+    <div class="container">
+        <div class="row">
+        <div class="col-lg-4 col-sm-12">
+            <div class="card" data-state="#about">
+        <div class="card-header">
+            <div class="card-cover" style="background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20201208/pngtree-beautiful-purple-blooming-christmas-snowflake-image_503982.jpg')"></div>
+            <img class="card-avatar" src="../Layout/img/male.png" />
+            <h1 class="card-fullname">0000</h1>
+        </div>
+        <div class="card-main">
+            <div class="card-section is-active" id="about">
+            <div class="card-content">
+                <div class="card-subtitle">聯絡資訊</div>
+                <p class="card-desc">
+                    <i class="fa-solid fa-phone"></i>&emsp;0900-000-000<br>
+                    <i class="fa-solid fa-envelope"></i>&emsp;0000@gmail.com
+                </p>
+            </div>
+            </div>
+        </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-12">
+            <div class="card" data-state="#about">
+        <div class="card-header">
+            <div class="card-cover" style="background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20201208/pngtree-beautiful-purple-blooming-christmas-snowflake-image_503982.jpg')"></div>
+            <img class="card-avatar" src="../Layout/img/male.png" />
+            <h1 class="card-fullname">0000</h1>
+        </div>
+        <div class="card-main">
+            <div class="card-section is-active" id="about">
+            <div class="card-content">
+                <div class="card-subtitle">聯絡資訊</div>
+                <p class="card-desc">
+                    <i class="fa-solid fa-phone"></i>&emsp;0900-000-000<br>
+                    <i class="fa-solid fa-envelope"></i>&emsp;0000@gmail.com
+                </p>
+            </div>
+            </div>
+        </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-12">
+            <div class="card" data-state="#about">
+        <div class="card-header">
+            <div class="card-cover" style="background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20201208/pngtree-beautiful-purple-blooming-christmas-snowflake-image_503982.jpg')"></div>
+            <img class="card-avatar" src="../Layout/img/male.png" />
+            <h1 class="card-fullname">0000</h1>
+        </div>
+        <div class="card-main">
+            <div class="card-section is-active" id="about">
+            <div class="card-content">
+                <div class="card-subtitle">聯絡資訊</div>
+                <p class="card-desc">
+                    <i class="fa-solid fa-phone"></i>&emsp;0900-000-000<br>
+                    <i class="fa-solid fa-envelope"></i>&emsp;0000@gmail.com
+                </p>
+            </div>
+            </div>
+        </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-12">
+            <div class="card" data-state="#about">
+        <div class="card-header">
+            <div class="card-cover" style="background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20201208/pngtree-beautiful-purple-blooming-christmas-snowflake-image_503982.jpg')"></div>
+            <img class="card-avatar" src="../Layout/img/male.png" />
+            <h1 class="card-fullname">0000</h1>
+        </div>
+        <div class="card-main">
+            <div class="card-section is-active" id="about">
+            <div class="card-content">
+                <div class="card-subtitle">聯絡資訊</div>
+                <p class="card-desc">
+                    <i class="fa-solid fa-phone"></i>&emsp;0900-000-000<br>
+                    <i class="fa-solid fa-envelope"></i>&emsp;0000@gmail.com
+                </p>
+            </div>
+            </div>
+        </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-12">
+            <div class="card" data-state="#about">
+        <div class="card-header">
+            <div class="card-cover" style="background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20201208/pngtree-beautiful-purple-blooming-christmas-snowflake-image_503982.jpg')"></div>
+            <img class="card-avatar" src="../Layout/img/male.png" />
+            <h1 class="card-fullname">0000</h1>
+        </div>
+        <div class="card-main">
+            <div class="card-section is-active" id="about">
+            <div class="card-content">
+                <div class="card-subtitle">聯絡資訊</div>
+                <p class="card-desc">
+                    <i class="fa-solid fa-phone"></i>&emsp;0900-000-000<br>
+                    <i class="fa-solid fa-envelope"></i>&emsp;0000@gmail.com
+                </p>
+            </div>
+            </div>
+        </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-12">
+            <div class="card" data-state="#about">
+        <div class="card-header">
+            <div class="card-cover" style="background-image: url('https://png.pngtree.com/thumb_back/fh260/background/20201208/pngtree-beautiful-purple-blooming-christmas-snowflake-image_503982.jpg')"></div>
+            <img class="card-avatar" src="../Layout/img/male.png" />
+            <h1 class="card-fullname">0000</h1>
+        </div>
+        <div class="card-main">
+            <div class="card-section is-active" id="about">
+            <div class="card-content">
+                <div class="card-subtitle">聯絡資訊</div>
+                <p class="card-desc">
+                    <i class="fa-solid fa-phone"></i>&emsp;0900-000-000<br>
+                    <i class="fa-solid fa-envelope"></i>&emsp;0000@gmail.com
+                </p>
+            </div>
+            </div>
+        </div>
+            </div>
+    </div>
+    </div>
+        
+    </div>
+    <LayoutFooter />
+</template>
+
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+.col-lg-4{
+    margin-top: 20px;
+}
+body {
+  color: #2b2c48;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  min-height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 20px;
+}
+
+.card {
+  max-width: 340px;
+  margin: auto;
+  overflow-y: auto;
+  position: relative;
+  z-index: 1;
+  overflow-x: hidden;
+  background-color: rgba(255, 255, 255, 1);
+  display: flex;
+  transition: 0.3s;
+  flex-direction: column;
+  border-radius: 10px;
+  box-shadow: 0 0 0 8px rgba(255, 255, 255, 0.2);
+}
+
+.card[data-state="#about"] {
+  height: 280px;
+  .card-main {
+    padding-top: 0;
+  }
+}
+
+.card.is-active {
+  .card-header {
+    height: 80px;
+  }
+
+  .card-cover {
+    height: 100px;
+    top: -50px;
+  }
+
+  .card-avatar {
+    transform: none;
+    left: 20px;
+    width: 50px;
+    height: 50px;
+    bottom: 10px;
+  }
+
+  .card-fullname,
+  .card-jobtitle {
+    left: 86px;
+    transform: none;
+  }
+
+  .card-fullname {
+    bottom: 18px;
+    font-size: 19px;
+  }
+}
+
+.card-header {
+  position: relative;
+  display: flex;
+  height: 190px;
+  flex-shrink: 0;
+  width: 100%;
+  transition: 0.3s;
+
+  * {
+    transition: 0.3s;
+  }
+}
+
+.card-cover {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  height: 160px;
+  top: -20%;
+  left: 0;
+  will-change: top;
+  background-size: cover;
+  background-position: center;
+  filter: blur(30px);
+  transform: scale(1.2);
+  transition: 0.5s;
+}
+
+.card-avatar {
+  width: 100px;
+  height: 100px;
+  box-shadow: 0 8px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  object-position: center;
+  object-fit: cover;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%) translateY(-64px);
+}
+
+.card-fullname {
+  position: absolute;
+  bottom: 0;
+  font-size: 22px;
+  font-weight: 700;
+  text-align: center;
+  white-space: nowrap;
+  transform: translateY(-10px) translateX(-50%);
+  left: 50%;
+}
+
+.card-main {
+  position: relative;
+  flex: 1;
+  display: flex;
+  padding-top: 10px;
+  flex-direction: column;
+}
+
+.card-subtitle {
+  font-weight: 700;
+  font-size: 13px;
+  margin-bottom: 8px;
+}
+
+.card-content {
+  padding: 20px;
+}
+
+.card-desc {
+  line-height: 1.6;
+  color: #636b6f;
+  font-size: 14px;
+  margin: 0;
+  font-weight: 400;
+}
+
+
+.card-section {
+  display: none;
+  &.is-active {
+    display: block;
+    animation: fadeIn 0.6s both;
+  }
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+    transform: translatey(40px);
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+</style>
