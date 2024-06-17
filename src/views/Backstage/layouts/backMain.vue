0006af8bf59abedd46ba71da0ad0dc54215434fb
fgh11112 <fgh11112@gmail.com>
2024-06-17 14:09:14 +0800

    刪除金鑰

--- src/views/Backstage/layouts/backMain.vue
+++ src/views/Backstage/layouts/backMain.vue
@@ -615,40 +615,6 @@ function drawChart8() {
 }
 // ==========================chart8 end=====================================
 
-// geochart
-// google.charts.load('current', {
-//     'packages': ['map'],
-//     // Note: you will need to get a mapsApiKey for your project.
-//     // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-//     'mapsApiKey': 'AIzaSyDETBnvF7lLVqzwOk_L2XXQ02DIkvJdrJ4'
-
-//     });
-//     google.charts.setOnLoadCallback(drawMap);
-
-//     function drawMap() {
-//       var data = google.visualization.arrayToDataTable([
-//         ['Country', 'Population'],
-//         ['China', 'China: 1,363,800,000'],
-//         ['India', 'India: 1,242,620,000'],
-//         ['US', 'US: 317,842,000'],
-//         ['Indonesia', 'Indonesia: 247,424,598'],
-//         ['Brazil', 'Brazil: 201,032,714'],
-//         ['Pakistan', 'Pakistan: 186,134,000'],
-//         ['Nigeria', 'Nigeria: 173,615,000'],
-//         ['Bangladesh', 'Bangladesh: 152,518,015'],
-//         ['Russia', 'Russia: 146,019,512'],
-//         ['Japan', 'Japan: 127,120,000']
-//       ]);
-
-//     var options = {
-//       showTooltip: true,
-//       showInfoWindow: true
-//     };
-
-//     var map = new google.visualization.Map(document.getElementById('chart'));
-
-//     map.draw(data, options);
-//   };
 </script>
 
 <style scoped>
